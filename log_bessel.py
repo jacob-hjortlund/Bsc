753811dee85e2b3df34b0acd0a5786335578f27f
jacob-hjortlund <jacob.hjortlund@gmail.com>
2020-03-06 10:57:09 +0100

    Vectorized log_bessel in v. Need to test and then vectorize for z.

--- log_bessel.py
+++ log_bessel.py
@@ -17,37 +17,41 @@ from scipy.integrate import quad, quad_vec, fixed_quad
 
 def rothwell_lead(v, z):
 
-	lead = 0.5*np.log(np.pi)-lg(v+0.5)-v*np.log(2*z)-z
+    lead = 0.5*np.log(np.pi)-lg(v+0.5)-v*np.log(2*z)-z
+    
+    lead[np.isinf(lead)] = -z + 0.5 * np.log(0.5*np.pi / z)
 
-	if np.isinf(lead):
-
-		lead = -z + 0.5 * np.log(0.5*np.pi / z)
-
-	return lead
+    return lead
 
 def inner_integral_rothwell(u, v, z):
 
-	n = 8
-	v_mhalf = v - 0.5
-	neg2v_m1 = -2. * v - 1.
-	beta = (2. * n ) / (2. * v + 1.)
+    n = 8
+    v_mhalf = v - 0.5
+    neg2v_m1 = -2. * v - 1.
+    beta = (2. * n ) / (2. * v + 1.)
 
-	uB = u**beta
-	inv_u = -1. / u
+    uB = u**beta
+    inv_u = -1. / u
 
-	first = beta * np.exp(-uB) * (2 * z + uB)**v_mhalf * u**(n-1)
-	second = np.exp(inv_u)
-	if second > 0:
-		second *= u**neg2v_m1
-		if np.is_inf(second):
-			second = np.exp(inv_u+neg2v_m1*np.log(u))
-		second *= (2. * z * u + 1)**v_mhalf
+    first = beta * np.exp(-uB) * (2 * z + uB)**v_mhalf * u**(n-1)
+    second = np.exp(inv_u)
+    if second > 0:
+        second *= u**neg2v_m1
+        if np.isinf(second):
+            second = np.exp(inv_u+neg2v_m1*np.log(u))
+        second *= (2. * z * u + 1)**v_mhalf
 
-	return first + second
+    return first + second
 
 def compute_log_integral(v, z):
-
-	return np.log(quad(inner_integral_rothwell, 0, 1, args=(v, z))[0])
+    
+    integral = np.zeros(np.shape(v))
+    
+    for i, v_tmp in enumerate(v):
+        
+        integral[i] = quad(inner_integral_rothwell,0,1,args=(v_tmp, z))[0]
+        
+    return np.log(integral)
 
 def rothwell(v, z):
 
@@ -70,21 +74,19 @@ def asymptotic_large_v(v, z):
 
 def asymptotic_large_z(v, z):
 
-	log_z = np.log(z)
-	base = 0.5 * (np.log(np.pi) - np.log(2) - log_z) - z
+    log_z = np.log(z)
+    base = 0.5 * (np.log(np.pi) - np.log(2) - log_z) - z
 
-	max_terms = 50
-	v_squared_4 = v * v * 4
-	a_k_z_k = 1
-	series_sum = 1
+    max_terms = 50
+    v_squared_4 = v * v * 4
+    a_k_z_k = 1
+    series_sum = 1
 
-	for k in range(1, max_terms+1):
-		a_k_z_k *= (v_squared_4-(2 * k -1)**2) / (k * z * 8)
-		series_sum += a_k_z_k
-		if np.abs(a_k_z_k) < 1e-8:
-			break
+    for k in range(1, max_terms+1):
+        a_k_z_k *= (v_squared_4-(2 * k -1)**2) / (k * z * 8)
+        series_sum += a_k_z_k
 
-	return base + np.log(series_sum)
+    return base + np.log(series_sum)
 
 # Trapezoidal rule for integral form as
 # defined in 
@@ -125,17 +127,18 @@ def method_indices(v, z):
 ###########################################
 
 def log_bessel_k(v, z):
+    
+    v = np.abs(v)
+    res = np.zeros(np.shape(v))
+    methods = [rothwell, asymptotic_large_z, asymptotic_large_v]
 
-	res = np.zeros(np.shape(v))
-	methods = [rothwell, asymptotic_large_z, asymptotic_large_v]
+    indeces = method_indices(v, z)
+    
+    for method, index in zip(methods, indeces):
 
-	indeces = method_indices(v, z)
-	
-	for method, index in zip(methods, indeces):
+        res[index] = method(v[index], z)
 
-		res[index] = method(v[index], z)
-
-	return res
+    return res, indeces
 
 
 
